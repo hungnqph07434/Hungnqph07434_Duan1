48032f790382ef5c3af748c5513cb21537a6a8e3
Hungnq <ngoquochunghh3@gmail.com>
2019-12-18 11:00:43 +0700

    acb

--- app/src/main/java/com/poly/hungnqph07434_duan1/datasql/SqlOpenHelper.java
+++ app/src/main/java/com/poly/hungnqph07434_duan1/datasql/SqlOpenHelper.java
@@ -130,7 +130,7 @@ public class SqlOpenHelper extends SQLiteOpenHelper {
 
     public List<NguoiChoi> getAllNguoiChoi(){
         List<NguoiChoi> nguoiChois = new ArrayList<>();
-        String SELECT = "SELECT * FROM nguoiChoi GROUP  by nguoiChoi.tenNguoiChoi ORDER by nguoiChoi.tenNguoiChoi ASC LIMIT 10";
+        String SELECT = "SELECT * FROM nguoiChoi GROUP  by nguoiChoi.Diem ORDER by nguoiChoi.Diem ASC LIMIT 10";
 
         SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
 
